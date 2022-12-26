c9c65d4fbbda6b18caf4e7003989b18f753cac66
LL <luxa@protonmail.com>
2022-12-26 15:18:09 +0100

    2022/ 	2023/

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$dbhost="localhost";
-$dbusername="XXX";
-$dbpassword="XXX";
-$dbdatabase = "XXX";
-$db = new mysqli ($dbhost,$dbusername,$dbpassword,$dbdatabase);?>
--- footer.php
+++
@@ -1,2 +0,0 @@
-
-<p style="margin-top: 50px; text-align: center; font-size: 13px;">Wunschbaumaktion 2.0 (c) 2021-2022 XXX</<p>
